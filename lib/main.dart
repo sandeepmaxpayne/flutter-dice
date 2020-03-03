a8535b9bb5ac9f26eefc9729884596f508e87433
DESKTOP-D7NMFO9\Sandeep <www.sandeepbabu45gmail.com>
2020-03-03 16:43:02 +0530

    Project completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,86 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
+          centerTitle: true,
           title: Text("Dice"),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: _DicePage(),
       ),
     ));
 
-class DicePage extends StatelessWidget {
+class _DicePage extends StatefulWidget {
+  @override
+  __DicePageState createState() => __DicePageState();
+}
+
+class __DicePageState extends State<_DicePage> {
+  int leftDiceNo = 5;
+  int rightDiceNo = 5;
+
+  String draw = "None";
+
+  void changeDiceState() {
+    setState(() {
+      leftDiceNo = Random().nextInt(6) + 1;
+      rightDiceNo = Random().nextInt(6) + 1;
+      if (leftDiceNo == rightDiceNo) {
+        draw = "Same Dice";
+      } else {
+        draw = "Different Dice";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Row(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Expanded(
-            flex: 1,
-            child: FlatButton(
-              child: Image.asset('images/dice1.png'),
+          Text(
+            'It\'s a $draw',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              fontSize: 30.0,
             ),
           ),
-          Expanded(
-            flex: 1,
-            child: FlatButton(
-              onPressed: () {
-                print("Paddu");
-              },
-              child: Image.asset('images/dice2.png'),
-            ),
+          SizedBox(
+            height: 25.0,
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: FlatButton(
+                  onPressed: () {
+                    // print("Left Button Pressed");
+                    //  print('dice: $leftDiceNo');
+                    changeDiceState();
+                  },
+                  child: Image.asset('images/dice$leftDiceNo.png'),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: FlatButton(
+                  onPressed: () {
+                    // print("Right  Button Pressed");
+                    changeDiceState();
+                  },
+                  child: Image.asset('images/dice$rightDiceNo.png'),
+                ),
+              ),
+            ],
           ),
         ],
       ),
     );
+    ;
   }
 }
